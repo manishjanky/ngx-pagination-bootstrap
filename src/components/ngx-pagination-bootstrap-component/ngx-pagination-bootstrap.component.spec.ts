24f10a68e8ffb6c23540bb01302c9d652b285443
Manish Kumar <m.kumar2@agilecockpit.com>
2018-01-24 19:48:35 +0530

    unit test cases written

--- src/components/ngx-pagination-bootstrap-component/ngx-pagination-bootstrap.component.spec.ts
+++ src/components/ngx-pagination-bootstrap-component/ngx-pagination-bootstrap.component.spec.ts
@@ -1,19 +1,20 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { PaginationComponent } from './ngx-pagination-bootstrap.component';
-import { FormsModule } from '@angular/forms';
+import { PaginationComponent } from "./ngx-pagination-bootstrap.component";
+import { FormsModule } from "@angular/forms";
 
-describe('PaginationComponent', () => {
+describe("PaginationComponent", () => {
   let component: PaginationComponent;
   let fixture: ComponentFixture<PaginationComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [FormsModule],
-      declarations: [ PaginationComponent ]
+  beforeEach(
+    async(() => {
+      TestBed.configureTestingModule({
+        imports: [FormsModule],
+        declarations: [PaginationComponent]
+      }).compileComponents();
     })
-    .compileComponents();
-  }));
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(PaginationComponent);
@@ -21,7 +22,112 @@ describe('PaginationComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("should calculate total page numbers", () => {
+    component.itemsCount = 35;
+    component.pageSize = 10;
+    component.calculatePageNumbers();
+    expect(component.totalPages).toEqual([1, 2, 3, 4]);
+  });
+
+  it("should navigate to page", () => {
+    component.pageSize = 10;
+    const pageNo = 2;
+    component.navigateToPage(pageNo);
+    expect(component.currentPage).toEqual(2);
+  });
+
+  it("should get current page items", () => {
+    component.pageSize = 10;
+    const pageNo = 2;
+    const $event = {
+      event,
+      pageNo,
+      pageSize: 10,
+      data: null
+    };
+    component.getPageItems($event);
+    expect($event.data).toEqual(null);
+  });
+
+  it("should get current page items when data not null", () => {
+    component.pageSize = 10;
+    component.data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 65];
+    component.currentPage = 2;
+    const pageNo = 2;
+    const $event = {
+      event,
+      pageNo,
+      pageSize: 10,
+      data: null
+    };
+    component.getPageItems($event);
+    expect($event.data).toEqual([1, 2, 3, 4, 65]);
+  });
+
+  it("should get current page data from function", () => {
+    component.pageSize = 10;
+    component.data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 65];
+    component.currentPage = 2;
+    expect(component.getCurrentPageData()).toEqual([1, 2, 3, 4, 65]);
+  });
+
+  it("should get next array of pages to display", () => {
+    component.itemsCount = 125;
+    component.pageSize = 10;
+    component.currentPage = 5;
+    component.calculatePageNumbers();
+    component.getNextPagesArrayToDisplay();
+    expect(component.pages).toEqual([3, 4, 5, 6, 7]);
+  });
+
+  it("should update the no of page on page size change", () => {
+    component.itemsCount = 125;
+    component.pageSize = 20;
+    component.currentPage = 5;
+    component.totalPages = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+    component.changePageSize();
+    expect(component.totalPages).toEqual([1, 2, 3, 4, 5, 6, 7]);
+  });
+
+  it("should navigate to next page", () => {
+    component.currentPage = 1;
+    component.nextPage();
+    expect(component.currentPage).toEqual(2);
+  });
+
+  it("should navigate to previous page", () => {
+    component.currentPage = 5;
+    component.previousPage();
+    expect(component.currentPage).toEqual(4);
+  });
+
+  it("should calculate current pages items range index", () => {
+    component.pageSize = 10;
+    component.currentPage = 5;
+    component.itemsCount = 125;
+    this.pages = [3, 4, 5, 6, 7];
+    component.calculateCurrentItemsRange();
+    expect(component.itemsRange.from).toEqual(41);
+    expect(component.itemsRange.to).toEqual(50);
+  });
+
+  it("should calculate current pages items range index", () => {
+    component.pageSize = 10;
+    component.itemsCount = 125;
+    component.currentPage = 7;
+    component.itemsCount = 125;
+    component.pages = [3, 4, 5, 6, 7];
+    component.calculateCurrentItemsRange();
+    expect(component.itemsRange.to).toEqual(125);
+  });
+
+  it("should set the items count based on data length", () => {
+    component.data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+    component.ngOnInit();
+    expect(component.itemsCount).toEqual(13);
   });
 });
